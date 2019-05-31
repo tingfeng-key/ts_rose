9a3c6f4f221932e0d51d7f0635b3ec35f83f3185
tingfeng-key <1770963469@qq.com>
2019-06-01 02:22:36 +0800

    update

--- src/rodio/mod.rs
+++ src/rodio/mod.rs
@@ -65,43 +65,11 @@ pub fn play_xinlifm(sender: Sender<String>, keyword: String) {
     use std::time::Duration;
 
     let _songs = xinlifm::get_songs_data(keyword);
-    let sink_clone = Arc::new(Mutex::new(sink));
-    let sink_clone_clone = sink_clone.clone();
-    let sink_musics = Arc::new(Mutex::new(musics));
-    let sink_musics_clone = sink_musics.clone();
-    thread::spawn(move || {
-        let mut last_played_music_name: String = "".to_string();
-        let mut current_playing_music_name: String = "".to_string();
-        let mut last_sink_len = 0;
-        loop {
-            let index = sink_musics.clone().lock().unwrap().len()
-                - sink_clone.clone().lock().unwrap().len();
-            current_playing_music_name = match sink_musics.clone().lock().unwrap().get(index) {
-                Some(music) => music.name.clone(),
-                None => "".to_string(),
-            };
-            if (last_played_music_name.ne(&current_playing_music_name)) {
-                last_played_music_name = current_playing_music_name.clone();
-                println!("当前歌曲: {}", current_playing_music_name);
-            }
-            sleep(Duration::from_secs(2));
-        }
-    });
 
     for song in _songs.members() {
         let sound_file_name =
             xinlifm::get_song_playurl_by_id("rodio".to_string(), song["id"].to_string());
-        sink_musics_clone.clone().lock().unwrap().push(Music {
-            name: song["title"].to_string(),
-            path: sound_file_name.clone(),
-        });
-        let file = std::fs::File::open(sound_file_name.clone()).unwrap();
-        //files.push(rodio::Decoder::new(BufReader::new(file)).unwrap());
-        sink_clone_clone
-            .lock()
-            .unwrap()
-            .append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-    }
 
-    sink_clone_clone.lock().unwrap().sleep_until_end();
+        sender.send(sound_file_name);
+    }
 }
