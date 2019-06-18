414ba113826e411b2ef7673bb48635021f36c301
tingfeng-key <1770963469@qq.com>
2019-06-18 20:16:31 +0800

    update

--- codec/src/main.rs
+++ codec/src/main.rs
@@ -1,3 +1,87 @@
+/*extern crate sample;
+use sample::{interpolate, signal, Frame, Signal};
+fn main() {
+    playback("resource/test.mp3");
+}
+fn playback(filename: &str) -> Result<(), Box<std::error::Error>> {
+
+
+    let mp3_data = std::fs::read(filename)?;
+
+    // Create the MP3 input stream.
+    let (header, samples) = puremp3::read_mp3(&mp3_data[..])?;
+
+    // Create the output audio stream using the cpal crate.
+    let device = cpal::default_output_device().ok_or("Failed to get default output device")?;
+    let out_format = device.default_output_format()?;
+    let event_loop = cpal::EventLoop::new();
+    let stream_id = event_loop.build_output_stream(&device, &out_format)?;
+    event_loop.play_stream(stream_id.clone());
+
+    // Use the sample crate to convert the MP3 stream to the output stream format.
+    let mut signal = signal::from_iter(samples.map(|sample| [sample.0, sample.1]));
+    let interp = interpolate::Linear::from_source(&mut signal);
+    let mut signal = signal.from_hz_to_hz(
+        interp,
+        header.sample_rate.hz().into(),
+        out_format.sample_rate.0.into(),
+    );
+
+    // Run the stream.
+    use cpal::{StreamData, UnknownTypeOutputBuffer};
+    event_loop.run(move |_, data| match data {
+        StreamData::Output {
+            buffer: UnknownTypeOutputBuffer::F32(mut buffer),
+        } => {
+            write_samples(&mut signal, &out_format, &mut buffer)
+        },
+
+        StreamData::Output {
+            buffer: UnknownTypeOutputBuffer::I16(mut buffer),
+        } => write_samples(&mut signal, &out_format, &mut buffer),
+
+        StreamData::Output {
+            buffer: UnknownTypeOutputBuffer::U16(mut buffer),
+        } => write_samples(&mut signal, &out_format, &mut buffer),
+
+        _ => unreachable!(),
+    });
+}
+
+/// Writes samples from the MP3 stream to the output audio stream.
+/// Generic because we don't know the format of the output stream until runtime.
+fn write_samples<S, T>(
+    in_signal: &mut S,
+    out_format: &cpal::Format,
+    out_buffer: &mut cpal::OutputBuffer<'_, T>,
+) where
+    S: sample::Signal<Frame = [f32; 2]>,
+    T: cpal::Sample + sample::Sample + sample::conv::FromSample<f32>,
+{
+    for sample in out_buffer.chunks_mut(out_format.channels as usize) {
+        if in_signal.is_exhausted() {
+            // Exit the app when the stream is complete.
+            std::process::exit(0);
+        }
+        let test = in_signal.next();
+        //let value = (test[0] + test[1])/2f32;
+        //println!("{}, {}, {}", test[0] ,test[1], value);
+        for out in sample.iter_mut() {
+            *out = test.get(0).unwrap();
+        }
+    }*/
+    /*for out_sample in out_buffer.chunks_mut(out_format.channels as usize) {
+        if in_signal.is_exhausted() {
+            // Exit the app when the stream is complete.
+            std::process::exit(0);
+        }
+
+        //let in_sample: [T; 2] = in_signal.next().map(sample::Sample::to_sample);
+        //println!("{:?}", in_signal.next());
+        //out_sample.copy_from_slice(&in_sample[..]);
+    }*/
+//}
+
 //use cpal::Sample;
 
 /*extern crate libc;
@@ -6,7 +90,7 @@ extern {
     //fn doubler(input: libc::c_int) -> libc::c_int;
     pub fn avpriv_io_delete(s: libc::c_char);
 }*/
-const LATENCY_MS: f32 = 150.0;
+//const LATENCY_MS: f32 = 150.0;
 fn main() {
     extern crate cpal;
     use cpal::{StreamData, UnknownTypeOutputBuffer};
@@ -89,13 +173,13 @@ fn main() {
     });
 }
 
-fn test_ffi() {
+/*fn test_ffi() {
     use std::path::Path;
     unsafe {
         //println!("{}", doubler(1));
         //avpriv_io_delete("./test")
     }
-}
+}*/
 /*
 fn get_mp3_duration() {
     let path = "resource/1559457768.mp3"; //1559457768
