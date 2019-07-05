67bc102208101a013032ac721dc070bbb691e13e
吴遵林 <wzl@wuzunlindeMBP.lan>
2019-07-06 00:07:47 +0800

    添加注释完成

--- ts_music_player/src/main.rs
+++ ts_music_player/src/main.rs
@@ -13,7 +13,7 @@ fn run_player() {
     use std::env;
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        panic!("arg error, first arg: n/x, two: song name");
+        panic!("the args error, first arg: n/x/q, two arg: song name");
     }
     let play_type: &str = &args[1];
     let keyword: String = args[2].to_string();
@@ -22,12 +22,15 @@ fn run_player() {
 
     let mut player = Player::new();
     match play_type {
+        //心理FM资源
         "x" => {
             site::play_xinlifm(player.get_download_list_sender(), keyword);
         }
+        //网易云资源
         "n" => {
             site::play_netease(player.get_download_list_sender(), keyword);
         }
+        //QQ音乐资源
         "q" => {
             site::play_tencent(player.get_download_list_sender(), keyword);
         }
