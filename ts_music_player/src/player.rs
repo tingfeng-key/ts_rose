7130e0da7a27c7d0ecf09789e1ac34f3dfa7c66e
吴遵林 <wzl@wuzunlindeMBP.lan>
2019-07-06 00:05:28 +0800

    添加注释

--- ts_music_player/src/player.rs
+++ ts_music_player/src/player.rs
@@ -10,9 +10,9 @@ use std::time::{Duration, Instant};
 pub struct MusicMeta {
     name: String,
     local_path: String,
-    duration: u64,
 }
 
+//下载结构体
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct Download {
@@ -29,6 +29,7 @@ pub struct Player {
     current_music_duration: Option<Duration>,
 }
 
+//播放器指令
 #[allow(dead_code)]
 enum PlayerCmd {
     Stop,
@@ -36,14 +37,8 @@ enum PlayerCmd {
     Exit,
     Hard,
 }
-#[allow(dead_code)]
-#[derive(Debug)]
-enum Error {
-    NoOutputDevice,
-    ChannelRecvTimeout,
-    ChannelRecvDisconnected,
-    RecvError,
-}
+
+//控制台指令
 #[allow(dead_code)]
 enum TerminalCmd {
     AddToList(MusicMeta),
@@ -54,87 +49,76 @@ enum TerminalCmd {
 #[allow(dead_code)]
 impl Player {
     pub fn new() -> Self {
+        //创建播放器线程异步通道
         let (player_sender, player_receiver) = channel();
+        //创建控制台线程异步通道
         let (terminal_sender, terminal_receiver) = channel();
+        //创建下载资源线程异步通道
         let (down_list_sender, down_list_receiver) = channel();
 
-        let musics: Arc<Mutex<Vec<MusicMeta>>> = Arc::new(Mutex::new(Vec::new())); //Arc::new<Mutex::new<Vec<MusicMeta>>>
+        //初始化播放列表
+        let musics: Arc<Mutex<Vec<MusicMeta>>> = Arc::new(Mutex::new(Vec::new()));
 
-        let terminal_sender_clone = terminal_sender.clone();
-        let duration = Duration::from_millis(1000_000);
-
-        let _player_thread_join_handle: thread::JoinHandle<Result<(), Error>> =
-            thread::spawn(move || -> Result<(), Error> {
-                let mut audio = crate::engine::audio::Audio::new();
-                let mut timer = Instant::now();
-                loop {
-                    //计时器，用于播放下一个音频
-                    match audio.duration {
-                        Some(d) => {
-                            if d.as_nanos() <= timer.elapsed().as_nanos() {
-                                timer = Instant::now();
-                                terminal_sender.send(TerminalCmd::Next).expect("error");
-                            }
-                        }
-                        None => {
+        //创建播放器线程
+        let _player_thread_join_handle = thread::spawn(move || {
+            let mut audio = crate::engine::audio::Audio::new();
+            let mut timer = Instant::now();
+            loop {
+                //计时器，用于播放下一个音频
+                match audio.duration {
+                    Some(d) => {
+                        if d.as_nanos() <= timer.elapsed().as_nanos() {
                             timer = Instant::now();
+                            terminal_sender.send(TerminalCmd::Next).expect("error");
                         }
                     }
-                    let recv_msg = player_receiver
-                        .recv_timeout(duration)
-                        .map_err(|e| match e {
-                            std::sync::mpsc::RecvTimeoutError::Timeout => Error::ChannelRecvTimeout,
-                            std::sync::mpsc::RecvTimeoutError::Disconnected => {
-                                Error::ChannelRecvDisconnected
-                            }
-                        })?;
-
-                    //接受消息
-                    match recv_msg {
-                        PlayerCmd::Play(music_meta) => {
-                            terminal_sender
-                                .send(TerminalCmd::NowPlay(music_meta.clone().name))
-                                .expect("error");
-                            audio.play(music_meta.clone().local_path.as_str());
-                            terminal_sender.send(TerminalCmd::Hard).expect("error");
-                        }
-                        PlayerCmd::Stop => {}
-                        PlayerCmd::Exit => {}
-                        PlayerCmd::Hard => {
-                            terminal_sender.send(TerminalCmd::Hard).expect("error");
-                        }
+                    None => {
+                        timer = Instant::now();
                     }
                 }
-            });
+                let recv_msg = player_receiver.recv_timeout(Duration::from_millis(10_000));
 
-        //下载资源线程
-        let _download_thread_join_handle = thread::spawn(move || -> Result<(), Error> {
-            loop {
-                let recv_msg = down_list_receiver
-                    .recv_timeout(duration)
-                    .map_err(|e| match e {
-                        std::sync::mpsc::RecvTimeoutError::Timeout => Error::ChannelRecvTimeout,
-                        std::sync::mpsc::RecvTimeoutError::Disconnected => {
-                            Error::ChannelRecvDisconnected
-                        }
-                    });
+                //接受消息
                 match recv_msg {
-                    Ok(download) => {
-                        let d: Download = download;
-                        let path = Self::download_file_by_url(d.url.as_str());
-                        let total_duration =
-                            mp3_duration::from_path(path.clone()).unwrap().as_secs();
-                        let music: MusicMeta = MusicMeta {
-                            name: d.name,
-                            local_path: path,
-                            duration: total_duration,
-                        };
-                        terminal_sender_clone
-                            .send(TerminalCmd::AddToList(music.clone()))
+                    //播放
+                    Ok(PlayerCmd::Play(music_meta)) => {
+                        terminal_sender
+                            .send(TerminalCmd::NowPlay(music_meta.clone().name))
                             .expect("error");
+                        audio.play(music_meta.clone().local_path.as_str());
+                        terminal_sender.send(TerminalCmd::Hard).expect("error");
+                    }
+                    //停止
+                    Ok(PlayerCmd::Stop) => {}
+                    //退出
+                    Ok(PlayerCmd::Exit) => {}
+                    //心跳
+                    OK(PlayerCmd::Hard) => {
+                        terminal_sender.send(TerminalCmd::Hard).expect("error");
                     }
                     Err(_) => {}
                 }
+            }
+        });
+
+        let terminal_sender_clone = terminal_sender.clone();
+
+        //创建下载资源线程
+        let _download_thread_join_handle = thread::spawn(move || loop {
+            let recv_msg = down_list_receiver.recv_timeout(duration);
+            match recv_msg {
+                Ok(download) => {
+                    let d: Download = download;
+                    let path = Self::download_file_by_url(d.url.as_str());
+                    let music: MusicMeta = MusicMeta {
+                        name: d.name,
+                        local_path: path,
+                    };
+                    terminal_sender_clone
+                        .send(TerminalCmd::AddToList(music.clone()))
+                        .expect("error");
+                }
+                Err(_) => {}
             }
         });
 
@@ -150,64 +134,67 @@ impl Player {
 
     //控制台输入输出
     pub fn term(&mut self) {
-        use console::Term;
-        let duration = Duration::from_millis(3_000); // 10 秒
-
-        let term = Term::stdout();
-
         loop {
             let recv_msg = self
                 .term_receiver
-                .recv_timeout(duration)
-                .map_err(|e| match e {
-                    std::sync::mpsc::RecvTimeoutError::Timeout => Error::ChannelRecvTimeout,
-                    std::sync::mpsc::RecvTimeoutError::Disconnected => {
-                        Error::ChannelRecvDisconnected
-                    }
-                });
+                .recv_timeout(Duration::from_millis(3_000));
             match recv_msg {
                 //添加歌曲
                 Ok(TerminalCmd::AddToList(music_meta)) => {
+                    Self::write_to_console(&format!(
+                        "添加歌曲《{}》至播放列表",
+                        music_meta.name
+                    ));
+
+                    //如果当前播放列表为空，自动播放
                     if self.musics.lock().unwrap().len() == 0 {
                         self.play_sender
                             .send(PlayerCmd::Play(music_meta.clone()))
                             .expect("send error");
                     }
+                    //添加歌曲到播放列表
                     self.musics.lock().unwrap().push(music_meta.clone());
-                    term.write_line(
-                        &format!("添加歌曲《{}》至播放列表", music_meta.name)
-                            .to_string(),
-                    )
-                    .unwrap();
                 }
+
                 //当前播放歌曲
                 Ok(TerminalCmd::NowPlay(song_name)) => {
-                    term.write_line(
-                        &format!("正在播放歌曲：《{}》", song_name).to_string(),
-                    )
-                    .unwrap();
+                    Self::write_to_console(&format!("正在播放歌曲：《{}》", song_name));
+
                     self.play_sender.send(PlayerCmd::Hard).expect("send error");
                 }
+
                 //下一首
                 Ok(TerminalCmd::Next) => {
+                    Self::write_to_console(&format!("下一首歌曲：《{}》", music.name));
+
+                    //获取下一首歌曲
                     let musics = self.musics.lock().unwrap();
                     self.current_music_index += 1;
                     let music = musics.get(self.current_music_index).unwrap();
-                    term.write_line(&format!("下一首歌曲：《{}》", music.name).to_string())
-                        .unwrap();
+
+                    //发送数据到播放器线程
                     self.play_sender
                         .send(PlayerCmd::Play(music.clone()))
                         .expect("send error");
                 }
+
                 //心跳
                 Ok(TerminalCmd::Hard) => {
                     self.play_sender.send(PlayerCmd::Hard).expect("send error");
                 }
+
                 Err(_) => {
                     println!("exit!");
                 }
             }
         }
+    }
+
+    //控制台输出
+    fn write_to_console(s: &str) {
+        use console::Term;
+        let term = Term::stdout();
+        term.write_line(s).unwrap();
     }
 
     //返回下载线程发送器
@@ -219,7 +206,6 @@ impl Player {
     //根据url下载音频
     fn download_file_by_url(play_url: &str) -> String {
         use reqwest::Url;
-        use std::error::Error;
         use std::fs;
         use std::io::copy;
         use std::time::SystemTime;
